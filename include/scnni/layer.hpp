1d7017dc0a2fc56fe1b50eae2d46697df27fa03c
xxayt <3494085277@qq.com>
2023-03-10 07:20:23 +0000

    update

--- include/scnni/layer.hpp
+++ include/scnni/layer.hpp
@@ -1,9 +1,9 @@
 /*
  * @Author: zzh
  * @Date: 2023-03-04
- * @LastEditTime: 2023-03-06 17:16:08
+ * @LastEditTime: 2023-03-06 17:50:51
  * @Description: 
- * @FilePath: /SCNNI/include/scnni/layer.hpp
+ * @FilePath: /code/scnni/include/scnni/layer.hpp
  */
 
 #ifndef SCNNI_LAYER_HPP_
@@ -20,14 +20,17 @@ class Layer {
     explicit Layer(std::string layer_name): layer_name_(std::move(layer_name)) {};
     virtual ~Layer() = default;
 
+
     /**
      * @description: Layer的前向计算
-     * @param std::vector<std::shared_ptr<Tensor<float>>>& input_blobs
-     * @param std::shared_ptr<std::vector<std::shared_ptr<Tensor<float>>>> output_blobs
+     * @param std::shared_ptr<Tensor<float>>& input_blobs
+     * @param std::shared_ptr<Tensor<float>> output_blobs
      * @return {*}
-     */
-    virtual auto Forward(const std::vector<std::shared_ptr<Tensor<float>>>& input_blobs, std::shared_ptr<std::vector<std::shared_ptr<Tensor<float>>>> output_blobs) const -> int; 
-    virtual auto Forward(const std::shared_ptr<Tensor<float>>& input_blobs, std::shared_ptr<Tensor<float>> output_blobs) const ->int;
+     */ 
+    virtual auto Forward(const std::shared_ptr<Tensor<float>>& input_blobs, 
+						std::shared_ptr<Tensor<float>> output_blobs) const ->int;
+    virtual auto Forward(const std::vector<std::shared_ptr<Tensor<float>>>& input_blobs,
+						std::shared_ptr<std::vector<std::shared_ptr<Tensor<float>>>> output_blobs) const -> int; 
 
     auto LayerName() const -> const std::string & { return this->layer_name_; }
 
@@ -40,4 +43,4 @@ class Layer {
 };
 
 } // namespace scnni
-#endif
+#endif  //SCNNI_LAYER_HPP_
