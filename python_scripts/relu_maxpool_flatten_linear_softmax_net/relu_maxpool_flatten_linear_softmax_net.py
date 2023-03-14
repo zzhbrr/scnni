0c264dc298b53d46b2754dd02630c32232b80667
xxayt <3494085277@qq.com>
2023-03-14 13:29:17 +0000

    add all test

--- python_scripts/relu_maxpool_flatten_linear_softmax_net/relu_maxpool_flatten_linear_softmax_net.py
+++ python_scripts/relu_maxpool_flatten_linear_softmax_net/relu_maxpool_flatten_linear_softmax_net.py
@@ -1,32 +1,33 @@
-'''
-Author: zzh
-Date: 2023-03-09
-LastEditTime: 2023-03-13 14:24:26
-Description: 
-FilePath: /scnni/python_scripts/test_net.py
-'''
-# https://github.com/pnnx/pnnx
 import torch
 import torch.nn as nn
 
 class TestNet(nn.Module):
     def __init__(self):
         super(TestNet, self).__init__()
+
         self.relu = nn.ReLU()
+        self.linear = nn.Linear(in_features = 5, out_features = 3, bias=True)
     def forward(self, x):
         x = self.relu(x)
         x = nn.MaxPool2d(2, 2)(x)
-        return torch.flatten(x, 1)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+        x = nn.Softmax(dim=1)(x)
+        return x
 
 
 if __name__ == '__main__':
     net = TestNet()
+    state_dict = torch.load('relu_maxpool_flatten_linear_softmax_net.pkl')
+    net.load_state_dict(state_dict)
     net.eval()
+
     # x = torch.tensor([1, 2, 3, -1, -2, 4, 5, 6, -7, -8, 12, 11])
     # x = torch.reshape(x, (1, 3, 2, 2))
-    x = torch.rand(1, 3, 4, 4)
+    # x = torch.rand(1, 3, 4, 4)
     # y = net(x)
     # print(y.shape)
+    torch.save(net.state_dict(), "relu_maxpool_flatten_linear_softmax_net.pkl")
     with torch.no_grad():
         mod = torch.jit.trace(net, x)
-        mod.save("relu_maxpool_flatten_softmax_net.pt")
+        mod.save("relu_maxpool_flatten_linear_softmax_net.pt")
